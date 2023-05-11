6aaf0055386f265d17880cb11c95c648df450967
AnooshkhaShetty <anooshkhashetty@gmail.com>
2023-05-10 17:34:06 -0700

    All workshop code

---
+++ chat-application-code/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import DirectMessaging from './DirectMessaging'
+import './App.css';
+
+const App = () => {
+  return (
+    <DirectMessaging/>
+  );
+}
+
+export default App;
